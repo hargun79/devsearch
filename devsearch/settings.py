fb7dd7cf073a0ae7479748b4612948598d7df553
hargun79 <hargunsinghsahni@gmail.com>
2021-12-25 18:39:19 +0530

    Removed Bug

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -201,7 +201,7 @@ AWS_S3_FILE_OVERWRITE = False
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'devsearch-bucket'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 
 if os.getcwd() == '/app':
